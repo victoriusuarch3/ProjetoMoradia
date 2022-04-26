6f5148e9cff9d001c185ed2cd1d826741e8fc083
Giovanni Sacchitiello <sacchitiellogiovanni@gmail.com>
2022-04-26 20:17:19 -0300

    criando solution e remanejando projeto

--- ProjetoMoradia.Console/Program.cs
+++ ProjetoMoradia.Console/Program.cs
@@ -9,7 +9,7 @@ public class Program
         Moradia.AdicionarMoradores(new Morador("Raimunda Brenda Isadora Santos","74904598261","09/03/1991"));
         Moradia.AdicionarMoradores(new Morador("Allana Priscila Aragão","28999665020","12/03/1963"));
         Moradia.AdicionarMoradores(new Morador("Manuel Victor Baptista","78944588724","01/01/1987"));
-        Moradia.AdicionarMoradores(new Morador("Augusto Sérgio Almada","69118437311","20/02/1971"));
+        Moradia.AdicionarMoradores(new Morador("Augusto Sérgio Almada","69118437311","12/02/1971"));
 
       /*Moradia Moradia = new Apartamento("Vila Frei Caneca", 68906465, 120, 3, 3, 8,1000);
         Moradia.AdicionarMoradores(new Morador("Jéssica Camila Bárbara Barbosa","09848955909","24/01/1948"));
